18a2480b51e0afa8e4bd2764c20aa16ca93e706d
VitaoL <victor.lustosacp@gmail.com>
2023-12-12 05:56:29 -0300

    location

--- lib/pages/list/list_widget.dart
+++ lib/pages/list/list_widget.dart
@@ -13,8 +13,10 @@ import '/flutter_flow/flutter_flow_widgets.dart';
 import 'list_model.dart';
 export 'list_model.dart';
 import 'sql_helper.dart';
+import 'package:geolocator/geolocator.dart';
 
-final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+final GlobalKey<ScaffoldMessengerState> _scaffoldMessengerKey =
+    GlobalKey<ScaffoldMessengerState>();
 
 class ListWidget extends StatefulWidget {
   const ListWidget({Key? key}) : super(key: key);
@@ -22,8 +24,6 @@ class ListWidget extends StatefulWidget {
   @override
   _ListWidgetState createState() => _ListWidgetState();
 }
-
-
 
 class ListaProdutos extends StatelessWidget {
   const ListaProdutos({Key? key}) : super(key: key);
@@ -45,6 +45,7 @@ class ListaProdutos extends StatelessWidget {
 
 class _ListWidgetState extends State<ListWidget> {
   List<Map<String, dynamic>> _produtos = [];
+  List<Map<String, dynamic>> _produtosFiltrados = [];
   bool _estaAtualizando = true;
 
   final TextEditingController _nomeController = TextEditingController();
@@ -72,13 +73,24 @@ class _ListWidgetState extends State<ListWidget> {
     final data = await SQLHelper.pegaProdutos();
     setState(() {
       _produtos = data;
+      _produtosFiltrados = _produtos; // Initialize filtered list
       _estaAtualizando = false;
+    });
+  }
+
+  void _filtrarProdutosPorNome(String filtro) {
+    setState(() {
+      _produtosFiltrados = _produtos
+          .where((produto) =>
+              produto['nome'].toLowerCase().contains(filtro.toLowerCase()))
+          .toList();
     });
   }
 
   void _mostraEdicao(int? id) async {
     if (id != null) {
-      final produtoExistente = _produtos.firstWhere((element) => element['id'] == id);
+      final produtoExistente =
+          _produtos.firstWhere((element) => element['id'] == id);
       _nomeController.text = produtoExistente['nome'];
       _descricaoController.text = produtoExistente['descricao'];
       _qrcodeController.text = produtoExistente['qrcode'].toString();
@@ -108,7 +120,8 @@ class _ListWidgetState extends State<ListWidget> {
             TextField(
               keyboardType: TextInputType.number,
               controller: _descricaoController,
-              decoration: const InputDecoration(hintText: 'Descrição do Produto'),
+              decoration:
+                  const InputDecoration(hintText: 'Descrição do Produto'),
             ),
             const SizedBox(height: 10),
             Row(
@@ -119,7 +132,8 @@ class _ListWidgetState extends State<ListWidget> {
                   child: TextField(
                     keyboardType: TextInputType.number,
                     controller: _qrcodeController,
-                    decoration: const InputDecoration(hintText: 'QRcode do Produto'),
+                    decoration:
+                        const InputDecoration(hintText: 'QRcode do Produto'),
                   ),
                 ),
                 IconButton(
@@ -134,7 +148,16 @@ class _ListWidgetState extends State<ListWidget> {
             TextField(
               keyboardType: TextInputType.number,
               controller: _qteController,
-              decoration: const InputDecoration(hintText: 'Quantidade do Produto'),
+              decoration:
+                  const InputDecoration(hintText: 'Quantidade do Produto'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Add your logic to get the current location here
+                _getCurrentLocation();
+              },
+              child: Text('Obter Posição Atual'),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
@@ -154,10 +177,49 @@ class _ListWidgetState extends State<ListWidget> {
                 Navigator.of(context).pop();
               },
               child: Text(id == null ? 'Adicionar' : 'Atualizar'),
-            )
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      // A posição contém as informações de latitude e longitude
+      double latitude = position.latitude;
+      double longitude = position.longitude;
+
+      // Faça algo com a latitude e longitude, por exemplo, exiba em um diálogo
+      _showLocationDialog(latitude, longitude);
+    } catch (e) {
+      print("Erro ao obter a posição atual: $e");
+      // Trate qualquer erro que possa ocorrer durante a obtenção da posição
+    }
+  }
+
+  void _showLocationDialog(double latitude, double longitude) {
+    // Exemplo simples de como mostrar a posição em um diálogo
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Posição Atual"),
+          content: Text("Latitude: $latitude\nLongitude: $longitude"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Fechar"),
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -244,9 +306,9 @@ class _ListWidgetState extends State<ListWidget> {
             'j1hdhf2v' /* Lista de itens */,
           ),
           style: FlutterFlowTheme.of(context).titleSmall.override(
-            fontFamily: 'Readex Pro',
-            color: Color(0xFFC1C1C1),
-          ),
+                fontFamily: 'Readex Pro',
+                color: Color(0xFFC1C1C1),
+              ),
         ),
         actions: [
           Padding(
@@ -267,71 +329,101 @@ class _ListWidgetState extends State<ListWidget> {
       ),
       body: _estaAtualizando
           ? const Center(
-        child: CircularProgressIndicator(),
-      )
+              child: CircularProgressIndicator(),
+            )
           : ListView.builder(
-        itemCount: _produtos.length,
-        itemBuilder: (context, index) => Card(
-          color: Colors.orange[50],
-          margin: const EdgeInsets.all(4),
-          child: ListTile(
-            title: Column(
-              children: [
-                Text("QRcode: ${_produtos[index]['qrcode']}",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 10,
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: () =>
-                          _mostraEdicao(_produtos[index]['id']),
-                    ),
-                    Text(_produtos[index]['nome'],
-                        style: const TextStyle(
-                          fontSize: 25,
+              itemCount: _produtos.length,
+              itemBuilder: (context, index) => Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                color: Colors.blue, // Cor de fundo azulado
+                margin: const EdgeInsets.all(8),
+                child: ListTile(
+                  contentPadding: const EdgeInsets.all(16),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "QRcode: ${_produtos[index]['qrcode']}",
+                        style: TextStyle(
                           fontWeight: FontWeight.bold,
-                        )),
-                  ],
+                          fontSize: 12,
+                          color: Colors.white, // Cor das letras branca
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () =>
+                                    _mostraEdicao(_produtos[index]['id']),
+                              ),
+                              Text(
+                                _produtos[index]['nome'],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  subtitle: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Descrição",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            _produtos[index]['descricao'],
+                            style: TextStyle(fontSize: 14, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Qte",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 12,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            _produtos[index]['qte'].toString(),
+                            style: TextStyle(fontSize: 14, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () => _apagaProduto(_produtos[index]['id']),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
             ),
-            subtitle: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    const Text("Descrição",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 10,
-                        )),
-                    Text(_produtos[index]['descricao']),
-                  ],
-                ),
-                Column(
-                  children: [
-                    const Text("Qte",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 10,
-                        )),
-                    Text(_produtos[index]['qte'].toString()),
-                  ],
-                ),
-                Column(),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () => _apagaProduto(_produtos[index]['id']),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 132, 0, 255),
         child: const Icon(
---
+++ lib/pages/q_rcode/QRScannerWidget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRScannerWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _QRScannerWidgetState();
+}
+
+class _QRScannerWidgetState extends State<QRScannerWidget> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: QRView(
+        key: qrKey,
+        onQRViewCreated: _onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderColor: Colors.blue,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: 300,
+        ),
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+
+    controller.scannedDataStream.listen((scanData) {
+      Navigator.pop(context, scanData.code);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
